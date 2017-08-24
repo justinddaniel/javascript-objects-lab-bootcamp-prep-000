154319d75d7558bda9d73c38cb7754b6160a309f
Justin Daniel <justinddaniel@gmail.com>
2017-08-24 01:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+function deleteFromObjectByKey(object, key) {
+  var object = { prop: 1 }
+  var newObj = delete object[key]
+  return newObj
+}
