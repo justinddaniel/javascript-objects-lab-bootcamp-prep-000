846188ef561172aa8c8c6a89bb00b2c85ca9cf79
Justin Daniel <justinddaniel@gmail.com>
2017-08-24 01:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var obj = { prop: 1 }
-  obj[key] = value
+  obj.key = value
   return obj
 }
